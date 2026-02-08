d1629abb0fa7ee7523d536a83235a56d95f6ec43
Mohammad Rasoul Abdolmaleki <128932603+mDevMaleki@users.noreply.github.com>
2026-02-08 11:44:01 +0330

    Add flexible DateTime JSON parsing

---
+++ PersianCustomers.EndPoints.WebApi/JsonConverters/NullableDateTimeJsonConverter.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Globalization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace PersianCustomers.EndPoints.WebApi.JsonConverters
+{
+    public class NullableDateTimeJsonConverter : JsonConverter<DateTime?>
+    {
+        private static readonly string[] SupportedFormats =
+        {
+            "yyyy-MM-dd",
+            "yyyy/MM/dd",
+            "yyyy-MM-ddTHH:mm:ss",
+            "yyyy-MM-ddTHH:mm:ss.FFF",
+            "yyyy-MM-ddTHH:mm:ssZ",
+            "yyyy-MM-ddTHH:mm:ss.FFFZ",
+            "O"
+        };
+
+        public override DateTime? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (reader.TokenType == JsonTokenType.Null)
+            {
+                return null;
+            }
+
+            if (reader.TokenType == JsonTokenType.String)
+            {
+                var value = reader.GetString();
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    return null;
+                }
+
+                if (DateTime.TryParseExact(value, SupportedFormats, CultureInfo.InvariantCulture,
+                        DateTimeStyles.RoundtripKind | DateTimeStyles.AllowWhiteSpaces, out var parsedExact))
+                {
+                    return parsedExact;
+                }
+
+                if (DateTime.TryParse(value, CultureInfo.InvariantCulture,
+                        DateTimeStyles.RoundtripKind | DateTimeStyles.AllowWhiteSpaces, out var parsed))
+                {
+                    return parsed;
+                }
+            }
+
+            throw new JsonException("Invalid date format.");
+        }
+
+        public override void Write(Utf8JsonWriter writer, DateTime? value, JsonSerializerOptions options)
+        {
+            if (value.HasValue)
+            {
+                writer.WriteStringValue(value.Value.ToString("O", CultureInfo.InvariantCulture));
+                return;
+            }
+
+            writer.WriteNullValue();
+        }
+    }
+}
